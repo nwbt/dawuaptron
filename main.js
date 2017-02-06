77a08182514ff0bea688bd5ca0553b391c29be40
Dan Catalano <dev@nwbt.co>
2017-02-05 21:21:11 -0700

    adapt UI to specification - sidebar
    added sidebar without styling complete ... need to figure out how to
    style input type file element

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let win;
 
 function createWindow () {
 	// Create the browser window.
-	win = new BrowserWindow({width: 1280, height: 800});
+	win = new BrowserWindow({width: 1280, height: 900});
 
 	// and load the index.html of the app.
 	win.loadURL(url.format({
